05bc7f5f31ee1c108344e5195c67b41e500763b8
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2019-07-31 14:53:41 +0300

    Edited the site.js file.

--- docs/scripts/site.js
+++ docs/scripts/site.js
@@ -1,3 +1,4 @@
+document.getElementsByTagName('body')[0].classList.remove('loaded');
 window.addEventListener('DOMContentLoaded', (e) => {
     document.getElementsByTagName('body')[0].classList.add('loaded');
 });
