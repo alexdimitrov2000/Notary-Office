9a3a9855e314a2b33b6f75a3fa33ef21c921d16b
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2019-08-07 00:03:53 +0300

    Removed the javascript file, which was adding a 'loaded' class to the body.

--- docs/scripts/site.js
+++
@@ -1,4 +0,0 @@
-document.getElementsByTagName('body')[0].classList.remove('loaded');
-window.addEventListener('DOMContentLoaded', (e) => {
-    document.getElementsByTagName('body')[0].classList.add('loaded');
-});
